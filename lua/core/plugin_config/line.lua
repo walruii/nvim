78ec941542c5ed1265070c620295d4892c2733db
walruii <walruiix@gmail.com>
2023-07-10 08:50:30 +0530

    finally moving from buffers to telescope idk if thats a good thing but i have felt faster

--- lua/core/plugin_config/line.lua
+++ lua/core/plugin_config/line.lua
@@ -1,17 +1,17 @@
-local bufferline = require('bufferline')
-bufferline.setup {
-    options = {
-        -- separator_style = "slant",
-        -- mode = "tabs",
-        offsets = {
-            filetype = "NvimTree",
-            text = " File Explorer",
-            text_align = 'left',
-            -- highlight = "Directory",
-            separator = true
-        }
-    }
-}
+-- local bufferline = require('bufferline')
+-- bufferline.setup {
+--     options = {
+--         -- separator_style = "slant",
+--         -- mode = "tabs",
+--         offsets = {
+--             filetype = "NvimTree",
+--             text = " File Explorer",
+--             text_align = 'left',
+--             -- highlight = "Directory",
+--             separator = true
+--         }
+--     }
+-- }
 
 require('lualine').setup {
     options = {
