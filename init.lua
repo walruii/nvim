1e4830165304cbb10d27e5265edc788a3311f6db
walruii <walruiix@gmail.com>
2023-07-31 20:15:31 +0530

    added the vscode detechtion

--- init.lua
+++ init.lua
@@ -1,4 +1,9 @@
-require("core.options")
-require("core.keymaps")
-require("core.plugins")
-require("core.plugin_config")
+if vim.g.vscode then
+    -- VSCode extension
+    require("core.vscode.settings")
+else
+    require("core.options")
+    require("core.keymaps")
+    require("core.plugins")
+    require("core.plugin_config")
+end
